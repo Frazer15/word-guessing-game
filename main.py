01b8440102e692d12ae2e8a0ef60e8dfbbaf0721
Frazer15 <91341353+Frazer15@users.noreply.github.com>
2023-02-14 13:35:12 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -2,32 +2,44 @@
 import random
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-def word_chooser(word_list):
-    word = random.choice(word_list)
-    randomized_word = []
-    for i in word:
-        randomized_word.append(i)
-    random.shuffle(randomized_word)
-    randomized_word = str(randomized_word)
-    wacky_word = randomized_word.strip(",'[]")
-    return wacky_word, word
 
-def tiers(word_list):
-    shuffled_word, real_word = word_chooser(word_list)
+def dict_warehouse(number):
+    dict3 = {tuple(['b','o','x']):["box"], tuple['j','o','y']:['joy'], tuple(['e','y','e']):['eye'],
+             tuple(['l','i','p']):["lip"]}
+    allDict = {3:dict3}
+    return allDict[number]
+
+def smart_dict_way(dict):
+    k3y = dict.keys()
+    val = dict[k3y]
+    k3y = list(k3y)
+    random.shuffle(k3y)
+    return k3y, val
+
+def tiers(dict):
+    shuffled_word, real_word = smart_dict_way(dict)
     print(shuffled_word)
     guessed_word = input("what is the word on the screen unscrambled")
-    if guessed_word == real_word:
-        print("good job")
-    else:
+    a = False
+    for z in real_word:
+        if guessed_word == real_word:
+            print("good job")
+            a = True
+        else:
+            pass
+    if a == False
         print('bad job, the word was ' + real_word)
+    else:
+        pass
+
 
 def main():
     # Use a breakpoint in the code line below to debug your script.
-    iterable = 0
-    word_list_3 = ["box","joy","eye","lip","rot","yet","cot","you","few","tie","men","gin","mix"]
-    while iterable < 5:
-        tiers(word_list_3)
-        iterable += 1
+    iterable = 3
+    for z in range(0,5):
+        word_list = dict_warehouse(iterable)
+        new_list = tiers(word_list)
+    iterable += 1
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
--- timer.py
+++ timer.py
@@ -2,22 +2,24 @@
 import time
 from threading import Timer
 
+import main
+
 
 def display(msg):
     print(msg + ' ' + time.strftime('%H:%M:%S'))
 
 
 ##Basic timer
-def run_once():
+def run_once(word_is_guessed):
     display('run_once:')
-    t = Timer(10, display, ['Timeout:'])
+    t = Timer(10, main.main(), ['you were to slow'])
     t.start()  # Here run is called
-
+    if word_is_guessed:
+        t.cancel()
+    else:
+        pass
 
 run_once()
-if word_is_guessed:
-    t.cancel()
-else:
-    pass
+
 ##Runs immediately and once
 print('Waiting.....')
